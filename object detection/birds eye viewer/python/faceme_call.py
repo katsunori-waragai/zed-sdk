97ea204dfd63a432f6d70afa4bc44cb8be1dfacc
katsunori-waragai <huyuhiko1128@gmail.com>
2024-02-19 18:49:38 +0900

    update faceme_wrapper.py faceme_ca..py

--- object detection/birds eye viewer/python/faceme_call.py
+++ object detection/birds eye viewer/python/faceme_call.py
@@ -1,6 +1,46 @@
+from pathlib import Path
+from pprint import pprint
+
 import cv2
 
 import faceme_wrapper
+
+def process_image_dir(img_dir: Path, out_dir: Path):
+    names = list(sorted(img_dir.glob("*.jpg")))  # ascii, Unicode multi byte names
+    print(list(names))
+    print(list(p.name for p in names))
+    basenames = [p.stem for p in names]
+    cvimgs = [cv2.imread(str(p)) for p in names]
+    t0 = cv2.getTickCount()
+    recognize_results, search_results = faceme_wrapper.process_images(cvimgs)
+    t1 = cv2.getTickCount()
+    used = (t1 - t0) / cv2.getTickFrequency()
+    print(f"{used=}")
+    for recognized, searched in zip(recognize_results, search_results):
+        print(f"{recognized.keys()=}")
+        print(f"{recognized['imageIndex']=} {recognized['boundingBox']=}")
+        print(f"{searched=}")
+        print("----")
+    pprint(search_results)
+    print("done search_result")
+    assert len(recognize_results) == len(search_results)
+    for basename, searched in zip(basenames, search_results):
+        print("----------------")
+        print(f"{basename=}")
+        pprint(searched)
+    # imageIndex が同じ画像の範囲でフィルタリングして、その範囲にあるrecognized, searched の結果をあわせて描画する
+    for i in range(len(names)):
+        outname = out_dir / f"{names[i].name}"
+        oimg = cvimgs[i]
+        indexes = [j for j, recognized in enumerate(recognize_results) if recognized["imageIndex"] == i]
+        print(f"{i=} {indexes=}")
+        for j in indexes:
+            pt1, pt2 = recognize_results[j]["boundingBox"]
+            cv2.rectangle(oimg, pt1, pt2, (0, 255, 0), thickness=3)
+            print(f"{j=} {search_results[j]=}")
+            person = search_results[j][1][0]["name"] if search_results[j][1] else ""
+            oimg = faceme_wrapper.putText_utf(oimg, unicode_text=person, org=pt1, font_size=36, color=(255, 0, 0))
+        cv2.imwrite(str(outname), oimg)
 
 if __name__ == "__main__":
     import argparse
@@ -15,7 +55,7 @@ if __name__ == "__main__":
         cap = cv2.VideoCapture(0)
         while True:
             r, img = cap.read()
-            recognize_results, search_results = faceme_wrapper.process_image(img)
+            recognize_results, search_results = faceme_wrapper.process_images([img])
             print(faceme_wrapper.bbox_and_name(recognize_results, search_results))
             out_cvimg = img
             out_cvimg = faceme_wrapper.draw_recognized(out_cvimg, recognize_results, search_results)
@@ -24,9 +64,14 @@ if __name__ == "__main__":
             key = cv2.waitKey(1)
             if key & 0xff == ord('q'):
                 break
+    elif Path(args.src).is_dir():
+        img_dir = Path(args.src)
+        out_dir = Path("output")
+        out_dir.mkdir(exist_ok=True)
+        process_image_dir(img_dir, out_dir)
     else:
         img = cv2.imread(args.src)
-        recognize_results, search_results = faceme_wrapper.process_image(img)
+        recognize_results, search_results = faceme_wrapper.process_images([img])
         print(faceme_wrapper.bbox_and_name(recognize_results, search_results))
         out_cvimg = img
         out_cvimg = faceme_wrapper.draw_recognized(out_cvimg, recognize_results, search_results)
