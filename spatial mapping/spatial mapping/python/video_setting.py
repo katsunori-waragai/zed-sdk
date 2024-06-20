7a981e140916c784e175b4d6565b8e0308679127
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-06-20 14:27:59 +0900

    Camera() datamembers

--- spatial mapping/spatial mapping/python/video_setting.py
+++ spatial mapping/spatial mapping/python/video_setting.py
@@ -2,5 +2,15 @@ import inspect
 
 import pyzed.sl as sl
 
-for k, v in inspect.getmembers(sl.VIDEO_SETTINGS):
-    print(f"{k}: {v}")
+# for k, v in inspect.getmembers(sl.VIDEO_SETTINGS):
+#     print(f"{k}: {v}")
+#     for k2, v2 in inspect.getmembers(v):
+#         print(f"\t{k2}: {v2}")
+
+zed = sl.Camera()
+for k, v in inspect.getmembers(zed):
+    if k.find("__") == -1:
+        print(f"{k}: {v}")
+        for k2, v2 in inspect.getmembers(v):
+            if k2.find("__") == -1:
+                print(f"\t{k2}: {v2}")
