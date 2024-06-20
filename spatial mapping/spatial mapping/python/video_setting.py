dfebc1d2afed5b4c2cf517fe8b4e04d59964e09e
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-06-20 14:52:52 +0900

    if continue

--- spatial mapping/spatial mapping/python/video_setting.py
+++ spatial mapping/spatial mapping/python/video_setting.py
@@ -2,15 +2,23 @@ import inspect
 
 import pyzed.sl as sl
 
-# for k, v in inspect.getmembers(sl.VIDEO_SETTINGS):
-#     print(f"{k}: {v}")
-#     for k2, v2 in inspect.getmembers(v):
-#         print(f"\t{k2}: {v2}")
-
 zed = sl.Camera()
 for k, v in inspect.getmembers(zed):
-    if k.find("__") == -1:
-        print(f"{k}: {v}")
-        for k2, v2 in inspect.getmembers(v):
-            if k2.find("__") == -1:
-                print(f"\t{k2}: {v2}")
+    if k.find("__") > -1:
+        continue
+    print("###")
+    if inspect.ismethod(v) or inspect.isfunction(v):
+        print(f"\t{k}: {inspect.getdoc(v)}")
+    else:
+        print(f"\t{k}: {v} {inspect.getdoc(v)}")
+
+    for k2, v2 in inspect.getmembers(v):
+        if k2.find("__") > -1:
+            continue
+
+        if inspect.ismethod(v2) or inspect.isfunction(v2):
+            print(f"\t{k2}: {inspect.getdoc(v2)}")
+        else:
+            print(f"\t{k2}: {v2} {inspect.getdoc(v2)}")
+
+print(inspect.getdoc(zed.get_region_of_interest))
