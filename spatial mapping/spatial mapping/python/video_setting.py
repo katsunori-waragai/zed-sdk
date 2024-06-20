39aebc2abbe712ce7cf00c0611b52b2742a084e6
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-06-20 15:16:35 +0900

    simpified

--- spatial mapping/spatial mapping/python/video_setting.py
+++ spatial mapping/spatial mapping/python/video_setting.py
@@ -1,3 +1,6 @@
+"""
+inspect.getdoc()
+"""
 import inspect
 
 import pyzed.sl as sl
@@ -7,12 +10,14 @@ for k, v in inspect.getmembers(zed):
     if k.find("__") > -1:
         continue
     print("###")
-    print(f"\t{k}: {inspect.getdoc(v)}")
+    print(f"{inspect.getdoc(v)}")
 
     for k2, v2 in inspect.getmembers(v):
         if k2.find("__") > -1:
             continue
 
-        print(f"\t{k2}: {inspect.getdoc(v2)}")
+        print(f"\t{inspect.getdoc(v2)}")
 
-print(inspect.getdoc(zed.get_region_of_interest))
+for k, v in  inspect.getmembers(zed):
+    if k.find("region") > -1:
+        print(f"{inspect.getdoc(v)}")
